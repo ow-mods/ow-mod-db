bbae09eede0f4fb92554255403a9326c3c87752d
Ricardo Lopes <raicuparta@gmail.com>
2020-05-10 21:51:29 +0200

    Fix imports

--- fetch-mods/index.ts
+++ fetch-mods/index.ts
@@ -1,5 +1,5 @@
-import core from '@actions/core';
-import github from '@actions/github';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 import axios from 'axios';
 
 const MANIFEST_URL_BASE = 'https://raw.githubusercontent.com';
