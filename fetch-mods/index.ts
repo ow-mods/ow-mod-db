3dd60ec4c4e16b67a24131cf718ce869b80384e6
Raicuparta <raicuparta@gmail.com>
2021-10-12 18:24:16 +0200

    Prevent whole thing from crashing when one mod is broken

--- fetch-mods/index.ts
+++ fetch-mods/index.ts
@@ -1,23 +1,23 @@
-import * as core from '@actions/core';
-import { getOctokit } from '@actions/github';
-import axios from 'axios';
+import * as core from "@actions/core";
+import { getOctokit } from "@actions/github";
+import axios from "axios";
 
-const RAW_URL_BASE = 'https://raw.githubusercontent.com';
-const REPO_URL_BASE = 'https://github.com';
+const RAW_URL_BASE = "https://raw.githubusercontent.com";
+const REPO_URL_BASE = "https://github.com";
 const JSON_INDENT = 2;
 
 const managerRepo = {
-  owner: 'Raicuparta',
-  repo:'ow-mod-manager',
+  owner: "Raicuparta",
+  repo: "ow-mod-manager",
 };
 
 enum Input {
-  mods = 'mods',
-  gitHubToken = 'github-token',
+  mods = "mods",
+  gitHubToken = "github-token",
 }
 
 enum Output {
-  releases = 'releases',
+  releases = "releases",
 }
 
 async function run() {
@@ -26,45 +26,58 @@ async function run() {
     const gitHubToken = core.getInput(Input.gitHubToken);
     const octokit = getOctokit(gitHubToken);
 
-    const managerReleases = (await octokit.paginate(octokit.repos.listReleases, {
+    const managerReleases = await octokit.paginate(octokit.repos.listReleases, {
       ...managerRepo,
       per_page: 100,
-    }));
+    });
     const managerLatestRelease = managerReleases[0];
-    const managerDownloadCount = managerReleases.reduce((managerDownloadAccumulator, { assets }) => {
-      const assetsDownloadCount = assets
-        .filter(({ name }) => 
-          (name.endsWith('zip') && !name.includes('LEGACY'))
-          || name.endsWith('exe')
-        )
-        .reduce((assetsDownloadAccumulator, { download_count }) => {
-          return assetsDownloadAccumulator + download_count
-        }, 0);
+    const managerDownloadCount = managerReleases.reduce(
+      (managerDownloadAccumulator, { assets }) => {
+        const assetsDownloadCount = assets
+          .filter(
+            ({ name }) =>
+              (name.endsWith("zip") && !name.includes("LEGACY")) ||
+              name.endsWith("exe")
+          )
+          .reduce((assetsDownloadAccumulator, { download_count }) => {
+            return assetsDownloadAccumulator + download_count;
+          }, 0);
 
-      return managerDownloadAccumulator + assetsDownloadCount;
-    }, 0);
+        return managerDownloadAccumulator + assetsDownloadCount;
+      },
+      0
+    );
 
     const results = [];
     for (let mod of mods) {
-      const [owner, repo] = mod.repo.split('/');
+      const [owner, repo] = mod.repo.split("/");
 
-      const fullReleaseList = (await octokit.paginate(octokit.repos.listReleases, {
-        owner,
-        repo,
-        per_page: 100,
-      }));
-      const prereleaseList = fullReleaseList.filter(release => release.prerelease);
-      const releaseList = fullReleaseList.filter(release => !release.prerelease);
+      const fullReleaseList = await octokit.paginate(
+        octokit.repos.listReleases,
+        {
+          owner,
+          repo,
+          per_page: 100,
+        }
+      );
+      const prereleaseList = fullReleaseList.filter(
+        (release) => release.prerelease
+      );
+      const releaseList = fullReleaseList.filter(
+        (release) => !release.prerelease
+      );
 
       if (releaseList.length === 0) {
         continue;
       }
 
-      const manifestUrl = (await octokit.repos.getContent({
-        owner,
-        repo,
-        path: mod.manifest,
-      })).data.download_url
+      const manifestUrl = (
+        await octokit.repos.getContent({
+          owner,
+          repo,
+          path: mod.manifest,
+        })
+      ).data.download_url;
 
       const manifest: Manifest = (await axios(manifestUrl)).data;
 
@@ -77,12 +90,14 @@ async function run() {
       });
     }
 
-    function getCleanedUpReleases(releaseList: typeof managerLatestRelease[]): Release[] {
+    function getCleanedUpReleases(
+      releaseList: typeof managerLatestRelease[]
+    ): Release[] {
       return releaseList
         .filter(({ assets }) => assets.length > 0)
-        .map(release => {
+        .map((release) => {
           const asset = release.assets[0];
-    
+
           return {
             downloadUrl: asset.browser_download_url,
             downloadCount: asset.download_count,
@@ -91,44 +106,51 @@ async function run() {
         });
     }
 
-    const modReleases: Mod[] = results.map(({
-      repo,
-      releaseList,
-      manifest,
-      required,
-      prereleaseList,
-    }) => {
-      const releases = getCleanedUpReleases(releaseList);
-      const prereleases = getCleanedUpReleases(prereleaseList);
+    const modReleases: (Mod | null)[] = results.map(
+      ({ repo, releaseList, manifest, required, prereleaseList }) => {
+        try {
+          const releases = getCleanedUpReleases(releaseList);
+          const prereleases = getCleanedUpReleases(prereleaseList);
 
-      const totalDownloadCount = releases.reduce((accumulator, release) => {
-        return accumulator + release.downloadCount;
-      }, 0);
+          const totalDownloadCount = releases.reduce((accumulator, release) => {
+            return accumulator + release.downloadCount;
+          }, 0);
 
-      const latestRelease = releases[0];
-      const latestPrerelease = prereleases[0];
+          const latestRelease = releases[0];
+          const latestPrerelease = prereleases[0];
 
-      const modInfo: Mod = {
-        downloadUrl: latestRelease.downloadUrl,
-        downloadCount: totalDownloadCount,
-        repo,
-        manifest,
-        required,
-        version: latestRelease.version,
-        prerelease: (latestPrerelease ? {
-          version: latestPrerelease.version,
-          downloadUrl: latestPrerelease.downloadUrl,
-        } : undefined),
-      };
+          const modInfo: Mod = {
+            downloadUrl: latestRelease.downloadUrl,
+            downloadCount: totalDownloadCount,
+            repo,
+            manifest,
+            required,
+            version: latestRelease.version,
+            prerelease: latestPrerelease
+              ? {
+                  version: latestPrerelease.version,
+                  downloadUrl: latestPrerelease.downloadUrl,
+                }
+              : undefined,
+          };
 
-      return modInfo;
-    });
+          return modInfo;
+        } catch (error: any) {
+          core.error(error);
+          return null;
+        }
+      }
+    );
 
     const assets = managerLatestRelease.assets;
-    const zipAssets = assets.filter(asset => asset.name.endsWith('.zip'));
-    const legacyZipAsset = zipAssets.find(asset => asset.name.includes('LEGACY'));
-    const mainZipAsset = zipAssets.find(asset => !asset.name.includes('LEGACY'));
-    const exeAsset = assets.find(asset => asset.name.endsWith('.exe'));
+    const zipAssets = assets.filter((asset) => asset.name.endsWith(".zip"));
+    const legacyZipAsset = zipAssets.find((asset) =>
+      asset.name.includes("LEGACY")
+    );
+    const mainZipAsset = zipAssets.find(
+      (asset) => !asset.name.includes("LEGACY")
+    );
+    const exeAsset = assets.find((asset) => asset.name.endsWith(".exe"));
 
     const modDatabase = {
       modManager: {
@@ -144,10 +166,9 @@ async function run() {
     const databaseJson = JSON.stringify(modDatabase, null, JSON_INDENT);
 
     core.setOutput(Output.releases, databaseJson);
-
-  } catch (error) {
+  } catch (error: any) {
     core.setFailed(error.message);
-    console.log('error', error);
+    console.log("error", error);
   }
 }
 
