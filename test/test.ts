6df763599b976731545da0d8087fa70fa3a1e7f5
Raicuparta <raicuparta@gmail.com>
2022-03-25 11:24:24 +0100

    add gh-token file

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,5 @@
 import { fetchMods } from "../fetch-mods";
 import modsJson from "./mods.json";
+import gitHubToken from "./gh-token.json";
 
-fetchMods(JSON.stringify(modsJson), "ghp_XXX");
+fetchMods(JSON.stringify(modsJson), gitHubToken);
