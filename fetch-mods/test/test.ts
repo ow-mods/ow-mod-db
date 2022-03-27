86ad2593f8201974b754eafcb9e524d7cd48fd52
Ricardo <Raicuparta@users.noreply.github.com>
2022-03-27 12:27:00 +0200

    Send release descriptions to notification

--- fetch-mods/test/test.ts
+++ fetch-mods/test/test.ts
@@ -6,8 +6,13 @@ import previousDatabase from "./previous-database.json";
 import { getDiff } from "../get-diff";
 import { sendDiscordNotifications } from "../send-discord-notifications";
 
-// fetchMods(JSON.stringify(modsJson), ghToken);
+async function test() {
+  // const nextDatabase = await fetchMods(JSON.stringify(modsJson), ghToken);
+  // console.log("nextDatabase", JSON.stringify(nextDatabase, null, 2));
 
-const diff = getDiff(previousDatabase.releases, nextDatabase.releases);
+  const diff = getDiff(previousDatabase, nextDatabase);
 
-sendDiscordNotifications(discordHookUrl, diff);
+  sendDiscordNotifications(discordHookUrl, diff);
+}
+
+test();
