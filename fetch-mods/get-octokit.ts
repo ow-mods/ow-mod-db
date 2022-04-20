09d515f9352945d783d8d8ee2bd4cf9f43e48155
Raicuparta <raicuparta@gmail.com>
2022-04-20 13:25:25 +0200

    retry all

--- fetch-mods/get-octokit.ts
+++ fetch-mods/get-octokit.ts
@@ -3,7 +3,13 @@ import { retry } from "@octokit/plugin-retry";
 
 function createOctokit() {
   const OctokitWithPlugins = Octokit.plugin(retry);
-  return new OctokitWithPlugins();
+  return new OctokitWithPlugins({
+    retry: {
+      // Make it retry for everything, even 404s,
+      // since the GH API some times randomly returns 404 in the latest release.
+      doNotRetry: [],
+    },
+  });
 }
 
 let octokit: ReturnType<typeof createOctokit> | undefined;
