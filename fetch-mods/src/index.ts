2445e0ceae2e33d54e35dfddf36638a1da507f20
Raicuparta <Raicuparta@users.noreply.github.com>
2023-03-05 18:05:46 +0100

    log stuff

--- fetch-mods/src/index.ts
+++ fetch-mods/src/index.ts
@@ -91,6 +91,8 @@ async function run() {
     await fsp.readFile(core.getInput(Input.previousDatabaseFile))
   ).toString();
 
+  console.log("previousDatabaseJson", previousDatabaseJson);
+
   const previousDatabaseOutput: DatabaseOutput =
     JSON.parse(previousDatabaseJson);
 
