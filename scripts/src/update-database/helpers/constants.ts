6643a74aef6617284238e84c6a7fe004896ca4b4
Raicuparta <Raicuparta@users.noreply.github.com>
2023-03-13 14:14:45 +0100

    Fix thumbnail url base

--- scripts/src/update-database/helpers/constants.ts
+++ scripts/src/update-database/helpers/constants.ts
@@ -9,7 +9,7 @@ export const DATABASE_REPO_BRANCH = "master";
 
 export const GITHUB_RAW_CONTENT_URL = "https://raw.githubusercontent.com";
 
-export const THUMBNAIL_URL_BASE = `${GITHUB_RAW_CONTENT_URL}/${DATABASE_REPO.owner}${DATABASE_REPO.repo}/${DATABASE_REPO_BRANCH}/thumbnails`;
+export const THUMBNAIL_URL_BASE = `${GITHUB_RAW_CONTENT_URL}/${DATABASE_REPO.owner}/${DATABASE_REPO.repo}/${DATABASE_REPO_BRANCH}/thumbnails`;
 
 export const RELEASE_EXTENSION = "zip";
 
