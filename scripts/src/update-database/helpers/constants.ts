2b6f0cf6c928639bd99e0f3c3a450ded19e96f8e
Raicuparta <Raicuparta@users.noreply.github.com>
2023-03-13 15:53:59 +0100

    use staging page for thumbnails

--- scripts/src/update-database/helpers/constants.ts
+++ scripts/src/update-database/helpers/constants.ts
@@ -9,7 +9,8 @@ export const DATABASE_REPO_BRANCH = "master";
 
 export const GITHUB_RAW_CONTENT_URL = "https://raw.githubusercontent.com";
 
-export const THUMBNAIL_URL_BASE = `${GITHUB_RAW_CONTENT_URL}/${DATABASE_REPO.owner}/${DATABASE_REPO.repo}/${DATABASE_REPO_BRANCH}/thumbnails`;
+// export const THUMBNAIL_URL_BASE = `${GITHUB_RAW_CONTENT_URL}/${DATABASE_REPO.owner}/${DATABASE_REPO.repo}/${DATABASE_REPO_BRANCH}/thumbnails`;
+export const THUMBNAIL_URL_BASE = `https://raicuparta.github.io/ow-mod-db-staging/thumbnails`;
 
 export const RELEASE_EXTENSION = "zip";
 
