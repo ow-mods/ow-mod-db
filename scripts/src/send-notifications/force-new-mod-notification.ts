34971e20283fa62991efc514ddcd10a53d88f2a5
xen-42 <22628069+xen-42@users.noreply.github.com>
2026-04-22 01:41:16 +0200

    Add new workflow to force the new mod notification (#1172)

---
+++ scripts/src/send-notifications/force-new-mod-notification.ts
@@ -0,0 +1,59 @@
+import * as core from "@actions/core";
+import { sendDiscordNotifications } from "./send-discord-notifications.js";
+import type { DiffItem } from "./get-diff.js";
+import { promises as fsp } from "fs";
+import type { DatabaseOutput } from "../mod.js";
+import type { BaseMod } from "../mod.js";
+
+enum Input {
+    currentDatabaseFile = "current-database",
+    discordHookUrl = "discord-hook-url",
+    discordNewModRoleId = "discord-new-mod-role-id",
+    modUniqueId = "mod-unique-id",
+}
+
+async function run() {
+    try {
+        const previousDatabaseJson = (
+            await fsp.readFile(core.getInput(Input.currentDatabaseFile))
+        ).toString();
+
+        const previousDatabaseOutput: DatabaseOutput =
+            JSON.parse(previousDatabaseJson);
+    
+        const previousMods = [
+            ...previousDatabaseOutput.releases,
+            ...previousDatabaseOutput.alphaReleases,
+        ];
+
+        const newModUniqueID = core.getInput(Input.modUniqueId);
+
+        const newMod = previousMods.find((mod) => mod.uniqueName == newModUniqueID);
+
+        if (newMod === undefined) {
+            throw new Error(newModUniqueID + " was not found in the database");
+        }
+
+        const discordHookUrl = core.getInput(Input.discordHookUrl);
+
+        const diff: DiffItem[] = [];
+
+        diff.push({
+          diffType: "add",
+          nextMod: newMod as BaseMod,
+        });
+
+        sendDiscordNotifications(
+            discordHookUrl,
+            "",
+            core.getInput(Input.discordNewModRoleId),
+            diff,
+            {}
+        );
+    } catch (error) {
+        core.setFailed(`Error running workflow script: ${error}`);
+        console.log(`Error running workflow script: ${error}`);
+    }
+}
+
+run();
